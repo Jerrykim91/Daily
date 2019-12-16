123ef58e3a074bcca37aaea056f3affdedb9c15c
HYOWON KIM <52520780+Jerrykim91@users.noreply.github.com>
2019-12-16 21:30:30 +0900

    공책정리중

---
+++ DeepLearning/DL_basic_zerocoding/DL_basic00.py
@@ -0,0 +1,8 @@
+# 인공지능( 머신러닝, 딥러닝, 텐서플로우 )강좌 
+# python 3.6 (64bit)
+# - IDE : jupyter notebook( 모듈 단위의 작업)
+# - scipy, matplotlib, tensorflow
+# 교육과정 교과서 [https://www.kyohak.co.kr/2014textbooks/index.asp]
+# 필요 수학 ( 함수, 삼각함수, 행렬, 복소수, 방정식, 미분, 확통)
+
+# 
