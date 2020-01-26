d2e42067772159f392f62a3e07660d5f36f3462e
Jerrykim91 <sun4131@gmail.com>
2020-01-26 23:59:53 +0900

    Crawling_Part6_ing(3)_2020.01.26

--- Coding_Library/Book/4.Learn_web_crawlers_in_Python/Step02/Part06_08.py
+++ Coding_Library/Book/4.Learn_web_crawlers_in_Python/Step02/Part06_08.py
@@ -1,11 +1,11 @@
-# Part06_07.py
+# Part06_08.py
 # 여기서는 실행이 안된다 당황;;
 
 # import 
 import requests as rq
 
 
-# 실습 Part06_01 - 7
+# 실습 Part06_01 - 8
 # 헤더 가지고 오기 
 url = "https://blog.naver.com/pjt3591oo"
 
@@ -13,5 +13,3 @@ res = rq.get(url)
 
 print(res)
 print(res.status_code)
-
-# 응답 객체에서 에더를 딕셔너리 형태로 가지고 옴 
